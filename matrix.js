127d127fc93c0c16b257a6dfea74dc479d51878b
Brady <vanderbannond@gmail.com>
2018-01-24 16:48:40 -0800

    cleaned up code and added a couple specific column and row tests

--- matrix.js
+++ matrix.js
@@ -8,89 +8,16 @@ class Matrix {
     this.cols = new Array(this.rows[0].length).fill().map(function() {
     return [];
   });
-    // for (var k = 0; k < this.cols.length; k++) {
-    //   this.cols[k] = [];
-    // }
-
-
-// this.cols[0]
-    console.log(this.cols);
-    console.log(this.cols[1]);
-    console.log(this.cols[2]);
-    // console.log(this.cols[0] = []);
-    // console.log(this.cols[1] = []);
-    // console.log(this.cols[2] = []);
-    // create a new array with row[0].length empty arrays
     for (var i = 0; i < this.rows.length; i++) {
-      // console.log("row number ", i);
       for (var j = 0; j < this.rows[i].length; j++) {
-        // console.log("column number", j);
-        // console.log("column value", this.rows[i][j]);
-
         this.cols[j].push(this.rows[i][j]);
       }
     }
   }
 }
-// this.rows[i].length
 
 var matrixObj = new Matrix("9 8 7\n5 3 2\n6 6 7\n1 1 2\n2 2 2");
-console.log(matrixObj.rows, " = rows");
-console.log(matrixObj.cols, " = cols");
-// console.log(matrixObj.rows[0]);
-// console.log(matrixObj.rows[0][1]);
-// console.log(matrixObj.rows.length);
-// console.log(matrixObj.cols);
-
-
-
-//PSEUDO
-//input "string"
-//output rows key: string, columns key: string
-
-// obj with rows and columns keys
-// add to rows and columns keys by looping over string in different ways
-// //for loop for rows key
-// create rows[0]
-// add "1 2"
-// when hit newline marker
-// i++ (i will equal 1 to create next row)
-// create rows[1]
-// add "10 20"
-//
-// for loop for columns key
-// create columns[0]
-// add "1 10"
-// create columns[1]
-// add "2 20"
-// columns is harder to just take first numbers of line,
-// then second set of numbers in line, etc
-
-
-
-
-
-
-
-
-
-// function matrix(input) {
-//   var j = 0;
-//   var array = [];
-//   for (var i = 0; i <= input.length; i++) {
-//     if (input[i] === " " || i === input.length) {
-//       while (j < i) {
-//         array += input[j];
-//         j++;
-//         console.log(array, " this is the array");
-//         console.log(j, " this is j count");
-//       }
-//     }
-// }
-// return array;
-// }
-
-
-
-
-// console.log(matrix('1 2\n10 20'));
+console.log("rows = ", matrixObj.rows);
+console.log("columns = ", matrixObj.cols);
+console.log(matrixObj.rows[3]);
+console.log(matrixObj.cols[1]);
